fba7fff218e1fc5ba77a67ee73aacae5f11b13f6
Jevgenij <sssgustavsss@gmail.com>
2023-09-26 22:41:32 +0300

    create user module

---
+++ backend/models/model_User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const schema_User = new mongoose.Schema({
+    username: { type: String, required: true, unique: true, minLength: 3 },
+    email: { type: String, required: true, unique: true },
+    name: { type: String },
+    password: { type: String, required: true, minLength: 8 },
+    role: { type: String, default: "user" }
+})
+
+schema_User.set("toJSON", {
+    transform: (document, returnedObject) =>
+    {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.password
+    }
+})
+
+const model_User = mongoose.model("user", schema_User, "user")
+
+module.exports = model_User
