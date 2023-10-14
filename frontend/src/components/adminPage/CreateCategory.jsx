e29251df603b5deda9a7209f045845dc876260cb
Jevgenij <sssgustavsss@gmail.com>
2023-10-14 18:58:13 +0300

    create component

---
+++ frontend/src/components/adminPage/CreateCategory.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react"
+import { useCategoriesContext } from "../../contexts/ContextCategories.js"
+import categoriesServices from "../../services/categoriesServices.js"
+import { Container, Form, FloatingLabel, Button } from "react-bootstrap"
+import { useNotificationContext } from "../../contexts/ContextNotification.js"
+
+const CreateCategory = () =>
+{
+    const { fetchCategories } = useCategoriesContext()
+    const { showToast } = useNotificationContext()
+
+    const [categoriesForm, setCategoriesForm] = useState({
+        title: "",
+        description: "",
+        urlPath: "",
+        imagePath: ""
+    })
+
+    const handleOnSubmit = async (e) =>
+    {
+        e.preventDefault()
+        try
+        {
+            const newCategory = await categoriesServices.createCategory(categoriesForm)
+            setCategoriesForm({ title: "", description: "", urlPath: "", imagePath: "" })
+            fetchCategories()
+            return showToast(newCategory.notification, newCategory.type)
+        }
+        catch (err)
+        {
+            console.log(err)
+        }
+    }
+
+    return (
+        <Container>
+            <Form onSubmit={handleOnSubmit}>
+                <Form.Group>
+                    <FloatingLabel label="Title" className="mb-3">
+                        <Form.Control type="text" placeholder="Name" value={categoriesForm.title} onChange={(e) => setCategoriesForm({ ...categoriesForm, title: e.target.value })} required />
+                    </FloatingLabel>
+                </Form.Group>
+
+                <Form.Group>
+                    <FloatingLabel label="Description" className="mb-3">
+                        <Form.Control type="text" placeholder="Description" value={categoriesForm.description} required onChange={(e) => setCategoriesForm({ ...categoriesForm, description: e.target.value })} />
+                    </FloatingLabel>
+                </Form.Group>
+
+                <Form.Group>
+                    <FloatingLabel label="Url path" className="mb-3">
+                        <Form.Control type="text" placeholder="/categories/path name" value={categoriesForm.urlPath} required onChange={(e) => setCategoriesForm({ ...categoriesForm, urlPath: e.target.value })} />
+                    </FloatingLabel>
+                </Form.Group>
+
+                <Form.Group>
+                    <FloatingLabel label="Image path" className="mb-3">
+                        <Form.Control type="text" placeholder="/assets/images/image name" value={categoriesForm.imagePath} onChange={(e) => setCategoriesForm({ ...categoriesForm, imagePath: e.target.value })} />
+                    </FloatingLabel>
+                </Form.Group>
+                <Button as="input" type="submit" />
+            </Form>
+        </Container>
+    )
+}
+
+export default CreateCategory
