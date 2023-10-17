a4b8fb4340fe93fd0cdb9a314bd4fe55ab9b98b0
Jevgenij <sssgustavsss@gmail.com>
2023-10-17 23:38:03 +0300

    create basic footer

---
+++ frontend/src/components/common/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import "../../assets/styles/Footer.css"
+
+const Footer = () =>
+{
+    return (
+        <div className="Footer-container">
+            <h1>Footer</h1>
+        </div>
+    )
+}
+
+export default Footer
