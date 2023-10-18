934f03c072e6b28e71300368bbc8f360fb1529a8
Jevgenij <sssgustavsss@gmail.com>
2023-10-18 23:53:09 +0300

    modify

--- frontend/src/contexts/ContextUser.js
+++ frontend/src/contexts/ContextUser.js
@@ -1,5 +1,6 @@
 import { useContext, createContext, useState, useEffect } from "react"
 import jwt_decode from "jwt-decode"
+import adminService from "../services/adminService.js"
 
 const ContextUser = createContext()
 
@@ -9,29 +10,72 @@ export function UserProvider({ children })
 
     const [user, setUser] = useState(null)
 
+    const [adminRights, setAdminRights] = useState(false)
+
+    const checkAdminRights = async () =>
+    {
+        try
+        {
+            const admin = await adminService.adminPageAccess()
+            setAdminRights(admin.message === "Access granted")
+        }
+        catch (err)
+        {
+            console.log(err)
+        }
+    }
+
+    const logout = () =>
+    {
+        window.localStorage.removeItem("token")
+        setUser(null)
+        setAdminRights(null)
+    }
+
     //Checking is there is token in localstorage
     useEffect(() =>
     {
         const token = window.localStorage.getItem("token")
+
         if (token)
         {
-            const decodedToken = jwt_decode(token)
-            const expirationTime = decodedToken.exp * 1000
-
-            if (expirationTime < Date.now())
+            try
             {
+                const decodedToken = jwt_decode(token)
+                const currentTime = Date.now() / 1000
+
+                if (decodedToken.exp < currentTime)
+                {
+                    window.localStorage.removeItem("token")
+                    setUser(null)
+                }
+                else
+                {
+                    setUser(decodedToken)
+                }
+
+            }
+            catch (err)
+            {
+                console.error("Error decoding token:", err)
                 window.localStorage.removeItem("token")
                 setUser(null)
-            }
-            else
-            {
-                setUser(token)
             }
         }
     }, [])
 
+    //Checking for admin rights
+    useEffect(() =>
+    {
+        if (!user)
+        {
+            return
+        }
+        checkAdminRights()
+    }, [user])
+
     return (
-        <ContextUser.Provider value={{ user, setUser }} >
+        <ContextUser.Provider value={{ user, setUser, adminRights, logout }} >
             {children}
         </ ContextUser.Provider>
     )
