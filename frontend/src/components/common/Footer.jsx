d93254c2dd84cc31e186362e38e11383e9cc4f0e
Jevgenij <sssgustavsss@gmail.com>
2023-11-16 13:28:23 +0200

    update footer

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -1,13 +1,24 @@
 import React from "react"
+import { FaYoutube, FaFacebook, FaTwitter, FaLinkedinIn, FaInstagram } from "react-icons/fa"
+
 
 import "../../assets/styles/Footer.css"
 
 const Footer = () =>
 {
     return (
-        <div className="Footer-container">
-            <h1>Footer</h1>
-        </div>
+        <footer className="Footer-container" >
+            <div className="footer-text">
+                <p>&copy; {new Date().getFullYear()} Train Bliss</p>
+            </div>
+            <div className="footer-icons">
+                <a href="https:youtube.com"><FaYoutube className="footer-current-icon" /></a>
+                <a href="https:facebook.com"><FaFacebook className="footer-current-icon" /></a>
+                <a href="https:twitter.com"><FaTwitter className="footer-current-icon" /></a>
+                <a href="https:linkedIn.com"><FaLinkedinIn className="footer-current-icon" /></a>
+                <a href="https:instagram.com"><FaInstagram className="footer-current-icon" /></a>
+            </div>
+        </footer>
     )
 }
 
