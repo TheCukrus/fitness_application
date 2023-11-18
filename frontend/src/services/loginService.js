1e816fa702f89c9d11cac0cf5e625eeafe88d32f
Jevgenij <sssgustavsss@gmail.com>
2023-11-18 20:42:32 +0200

    creat login service

---
+++ frontend/src/services/loginService.js
@@ -0,0 +1,42 @@
+import axios from "axios"
+
+const baseUrl = "http://127.0.0.1:80/api/v1/login"
+
+//Extracting token from localStorage
+const getToken = () =>
+{
+    const tokenFromLocalstorage = window.localStorage.getItem("token")
+    return tokenFromLocalstorage ? `Bearer ${JSON.parse(tokenFromLocalstorage).token}` : null
+}
+
+//Seting token to directly from login component
+const setAuthorizationToken = (token) =>
+{
+    config.headers.authorization = token
+}
+
+//Config file
+const config = {
+    headers: {
+        "authorization": getToken()
+    }
+}
+
+
+//POST Login
+const authorization = async (data) =>
+{
+    try
+    {
+        const login = await axios.post(baseUrl, data)
+        return login.data
+    }
+    catch (err)
+    {
+        console.log(err)
+        return { notification: err.response.data.message, type: "error" }
+    }
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { authorization, setAuthorizationToken }
