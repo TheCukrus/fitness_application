abddb03be3844bb7f2d6120877e4ff56d5ac94dd
Jevgenij <sssgustavsss@gmail.com>
2023-11-24 12:53:26 +0200

    modify footer

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -1,22 +1,21 @@
 import React from "react"
 import { FaYoutube, FaFacebook, FaTwitter, FaLinkedinIn, FaInstagram } from "react-icons/fa"
 
-
-import "../../assets/styles/Footer.css"
+import s from "../../assets/styles/Footer.module.css"
 
 const Footer = () =>
 {
     return (
-        <footer className="Footer-container" >
-            <div className="footer-text">
+        <footer className={s.Footer_container} >
+            <div className={s.footer_text}>
                 <p>&copy; {new Date().getFullYear()} Train Bliss</p>
             </div>
-            <div className="footer-icons">
-                <a href="https:youtube.com"><FaYoutube className="footer-current-icon" /></a>
-                <a href="https:facebook.com"><FaFacebook className="footer-current-icon" /></a>
-                <a href="https:twitter.com"><FaTwitter className="footer-current-icon" /></a>
-                <a href="https:linkedIn.com"><FaLinkedinIn className="footer-current-icon" /></a>
-                <a href="https:instagram.com"><FaInstagram className="footer-current-icon" /></a>
+            <div className={s.footer_icons}>
+                <a href="https:youtube.com"><FaYoutube className={s.footer_current_icon} /></a>
+                <a href="https:facebook.com"><FaFacebook className={s.footer_current_icon} /></a>
+                <a href="https:twitter.com"><FaTwitter className={s.footer_current_icon} /></a>
+                <a href="https:linkedIn.com"><FaLinkedinIn className={s.footer_current_icon} /></a>
+                <a href="https:instagram.com"><FaInstagram className={s.footer_current_icon} /></a>
             </div>
         </footer>
     )
