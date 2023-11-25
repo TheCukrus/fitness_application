030e3c0e87035df5e1c62e69dcc14a11c700143c
Jevgenij <sssgustavsss@gmail.com>
2023-11-25 08:09:48 +0200

    modify service

--- frontend/src/services/adminService.js
+++ frontend/src/services/adminService.js
@@ -1,6 +1,15 @@
 import axios from "axios"
 
-const baseUrl = "http://127.0.0.1:80/api/v1/admin"
+let baseUrl
+
+if (process.env.NODE_ENV === "development")
+{
+    baseUrl = "http://127.0.0.1:80/api/v1/admin"
+}
+else
+{
+    baseUrl = "https://fitness-application-faws.onrender.com/api/v1/admin"
+}
 
 const getToken = () =>
 {
